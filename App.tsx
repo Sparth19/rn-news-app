a343aaa1a096e9180eb18b69f78bbd7cabf968b8
Parth Shekhaliya <parthshekhaliya18@gmail.com>
2024-05-30 11:10:27 +0530

    - completed env setup with debugger

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 import {Colors, FONTS, FONT_SIZE} from './src/themes/AppTheme';
+import 'react-native-devsettings/withAsyncStorage';
+
+const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
 const App: React.FC = () => {
+  console.log(NEWS_API_KEY);
   return (
     <SafeAreaView style={styles.center}>
       <Text style={styles.text}>Hello world</Text>
